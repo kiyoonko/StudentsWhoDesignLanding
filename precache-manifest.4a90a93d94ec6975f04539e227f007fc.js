f582b05cea5474fdfd87765fa8a7fad64b663cd2
Kiyoon Ko <kiyoonko@gmail.com>
2019-10-25 15:14:25 -0700

    Updates

---
+++ precache-manifest.4a90a93d94ec6975f04539e227f007fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515619840efbb4327ba763ae0c5b7c51",
+    "url": "/StudentsWhoDesignLanding/index.html"
+  },
+  {
+    "revision": "50f7251029bd6baea94b",
+    "url": "/StudentsWhoDesignLanding/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "b401913910e858a3790f",
+    "url": "/StudentsWhoDesignLanding/static/css/main.eaf1fd31.chunk.css"
+  },
+  {
+    "revision": "50f7251029bd6baea94b",
+    "url": "/StudentsWhoDesignLanding/static/js/2.041b9aa8.chunk.js"
+  },
+  {
+    "revision": "b401913910e858a3790f",
+    "url": "/StudentsWhoDesignLanding/static/js/main.94b9a54d.chunk.js"
+  },
+  {
+    "revision": "f1b0bd970870d581f2e2",
+    "url": "/StudentsWhoDesignLanding/static/js/runtime-main.58abd9ae.js"
+  },
+  {
+    "revision": "66fcc5ddf94238cab25f04b02b7fa274",
+    "url": "/StudentsWhoDesignLanding/static/media/ApplyButton.66fcc5dd.svg"
+  },
+  {
+    "revision": "b9da22f5af86748a620cc828829b21e8",
+    "url": "/StudentsWhoDesignLanding/static/media/background.b9da22f5.svg"
+  },
+  {
+    "revision": "01974e998d2f2e691e34439d58a14899",
+    "url": "/StudentsWhoDesignLanding/static/media/logo.01974e99.svg"
+  },
+  {
+    "revision": "4cd5855710b3a05f25cb8b219ddb23db",
+    "url": "/StudentsWhoDesignLanding/static/media/requestButton.4cd58557.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/StudentsWhoDesignLanding/precache-manifest.4a90a93d94ec6975f04539e227f007fc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StudentsWhoDesignLanding/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
